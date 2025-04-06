629b2882ace97974b72b6e60f0a5613bb1d73e5d
Vikanth <yvikanth817@gmail.com>
2025-04-06 15:47:40 +0530

    react-routing

--- Frontend/src/landing_page/signup/SignupPage.jsx
+++ Frontend/src/landing_page/signup/SignupPage.jsx
@@ -1,11 +1,10 @@
+import React from "react";
 import Signup from "./Signup";
 
 function SignupPage() {
     return ( 
-        <>
-            <Signup />
-        </>
-     );
+        <Signup />
+    );
 }
 
 export default SignupPage;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import LoginPage from '../src/landing_page/login/LoginPage.jsx'
 import SignupPage from '../src/landing_page/signup/SignupPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <LoginPage /> */}
-    <SignupPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )

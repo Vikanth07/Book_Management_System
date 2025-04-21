bbe7a420b3e289686b2778b1fbefe1fecfbf5553
Vikanth <yvikanth817@gmail.com>
2025-04-22 00:34:39 +0530

    checking auth

--- Backend/middlewares/AuthMiddleware.js
+++ Backend/middlewares/AuthMiddleware.js
@@ -5,15 +5,15 @@ const jwt = require("jsonwebtoken");
 module.exports.userVerification = (req, res) => {
   const token = req.cookies.token
   if (!token) {
-    return res.json({ status: false })
+    return res.json({ status: false, message: "Token not found" })
   }
   jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
     if (err) {
-     return res.json({ status: false })
+     return res.json({ status: false, message: "Token expired" })
     } else {
       const user = await UserModel.findById(data.id)
       if (user) return res.json({ status: true, user: user.username })
-      else return res.json({ status: false })
+      else return res.json({ status: false, message: "User not found" })
     }
   })
 }
